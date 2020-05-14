2a8efb1bae7ce62027205e18702fbe053842a045
joshskkim <josh.sk.kim@gmail.com>
2020-05-13 20:02:39 -0700

    Loads services from aws links

--- server/index.js
+++ server/index.js
@@ -6,26 +6,29 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(express.static(`${__dirname}/../public`));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const mediaProxy = createProxyMiddleware(
   {
-    target: 'http://localhost:8000',
+    target: 'http://3.23.132.230:8000/',
     changeOrigin: true,
   },
 );
 
-// const announcementsProxy = createProxyMiddleware(
-//   {
-//     target: 'http://localhost:4200',
-//     changeOrigin: true,
-//   },
-// );
+// const sidebarProxy = createProxyMiddleware({
+//   target: 'http://localhost:1991/',
+//   changeOrigin: true,
+// });
+
+const announcementsProxy = createProxyMiddleware(
+  {
+    target: 'http://3.17.138.9:8080',
+    changeOrigin: true,
+  },
+);
 
 const reviewsProxy = createProxyMiddleware(
   {
-    target: 'http://localhost:4200',
+    target: 'http://54.67.101.150:4200',
     changeOrigin: true,
   },
 );
@@ -36,16 +39,37 @@ app.use(
   mediaProxy,
 );
 
-// // proxy to announcements service
+// // proxy to siderbar service
 // app.use(
-//   'randomGame',
-//   announcementsProxy,
-// );
+//   '/mainBody',
+//   sidebarProxy);
+
+// proxy to announcements service for random game
+app.use(
+  '/randomGame',
+  announcementsProxy,
+);
+
+// proxy to announcements service for specific game
+app.use(
+  '/getGame',
+  announcementsProxy,
+);
+
+// proxy to announcements service to patch likes
+app.use(
+  '/updateLikes',
+  announcementsProxy,
+);
+
 
 // proxy to reviews service
 app.use(
   '/api/reviews/:id',
   reviewsProxy,
 );
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
