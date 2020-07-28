2b561d46a06fbcb3e7040705f21f5ce529864e0d
Joshua Kim <josh.sk.kim@gmail.com>
2020-07-28 15:56:44 -0700

    Edited links to deployed services

--- server/index.js
+++ server/index.js
@@ -9,26 +9,26 @@ app.use(express.static(`${__dirname}/../public`));
 
 const mediaProxy = createProxyMiddleware(
   {
-    target: 'http://3.23.132.230:8000',
+    target: 'http://18.219.209.226:8000',
     changeOrigin: true,
   },
 );
 
 const sidebarProxy = createProxyMiddleware({
-  target: 'http://52.14.114.30:1992',
+  target: 'http://18.191.250.26:1992',
   changeOrigin: true,
 });
 
 const announcementsProxy = createProxyMiddleware(
   {
-    target: 'http://3.17.138.9:8080',
+    target: 'http://18.217.243.249:8080',
     changeOrigin: true,
   },
 );
 
 const reviewsProxy = createProxyMiddleware(
   {
-    target: 'http://54.67.101.150:4200',
+    target: 'http://18.218.201.173:4200',
     changeOrigin: true,
   },
 );
